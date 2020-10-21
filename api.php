6551066813960a77857460127bf2fa0fa12aee5a
Angga-Azhari <m.dwi_aswangga_28rpl@student.smktelkom-mlg.sch.id>
2020-10-21 21:45:42 +0700

    Menambah pembagian hak akses

--- api.php
+++ api.php
@@ -4,23 +4,35 @@ use Illuminate\Http\Request;
 Route::post('/register', 'UserController@register');
 Route::post('/login', 'UserController@login');
 
-Route::group(['middleware' => ['jwt.verify']], function ()
-{
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::group(['middleware' => ['api.superadmin']], function()
+    {
+        Route::delete('/barang/{id_barang}', 'BarangController@destroy');
+        Route::delete('/customer/{id_pembeli}', 'CustomerController@destroy');
+        Route::delete('/transaksi/{id_transaksi}', 'TransaksiController@destroy');
+
+    });
+    Route::group(['middleware' => ['api.admin']], function()
+    {
+        Route::post('/barang', 'BarangController@store');
+        Route::put('/barang/{id_barang}', 'BarangController@update');
+
+        Route::post('/customer', 'CustomerController@store');
+        Route::put('/customer/{id_pembeli}', 'CustomerController@update');
+
+        Route::post('/transaksi', 'TransaksiController@store');
+        Route::put('/transaksi/{id_transaksi}', 'TransaksiController@update');
+
+    });
+
 
 Route::get('/barang', 'BarangController@show');
-Route::post('/barang', 'BarangController@store');
-Route::put('/barang/{id_barang}', 'BarangController@update');
-Route::delete('/barang/{id_barang}', 'BarangController@destroy');
 
-Route::post('/customer', 'CustomerController@store');
 Route::get('/customer', 'CustomerController@show');
 Route::get('/customer/{id_pembeli}', 'CustomerController@detail');
-Route::put('/customer/{id_pembeli}', 'CustomerController@update');
-Route::delete('/customer/{id_pembeli}', 'CustomerController@destroy');
 
 Route::get('/transaksi', 'TransaksiController@show');
 Route::get('/transaksi/{id_transaksi}', 'TransaksiController@detail');
-Route::post('/transaksi', 'TransaksiController@store');
-Route::put('/transaksi/{id_transaksi}', 'TransaksiController@update');
-Route::delete('/transaksi/{id_transaksi}', 'TransaksiController@destroy');
+
+
 });
